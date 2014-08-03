2b4852901f99c077ca060076825e5fe15408973c
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2014-08-04 00:58:44 +0200

    First commit

---
+++ sanei-core/lib/sanei.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# load external modules
+
+local __IMPORT__BASE_PATH="$SCRIPT_DIR/vendor/bash-modules/main/bash-modules/src/bash-modules"
+source ${SCRIPT_DIR}/vendor/bash-modules/main/bash-modules/src/import.sh arguments log
+
