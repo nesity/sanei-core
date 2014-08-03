93c2cc6bee906106aaaee66cc96f0a6c51c8b9e7
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2014-08-04 01:07:44 +0200

    test + gitignore

--- sanei-core/lib/sanei.sh
+++ sanei-core/lib/sanei.sh
@@ -2,6 +2,6 @@
 
 # load external modules
 
-local __IMPORT__BASE_PATH="$SCRIPT_DIR/vendor/bash-modules/main/bash-modules/src/bash-modules"
+__IMPORT__BASE_PATH="$SCRIPT_DIR/vendor/bash-modules/main/bash-modules/src/bash-modules"
 source ${SCRIPT_DIR}/vendor/bash-modules/main/bash-modules/src/import.sh arguments log
 
